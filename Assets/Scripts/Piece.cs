0daa4cdbe330f6c9e02b2b40d0df7461cd0d040e
Alistair Quinn <alistairaldo@hotmail.co.uk>
2025-11-27 22:06:24 +0000

    More ai generated slop

--- Assets/Scripts/Piece.cs
+++ Assets/Scripts/Piece.cs
@@ -2,5 +2,35 @@ using UnityEngine;
 
 public abstract class Piece : MonoBehaviour
 {
+    private Color originalColour;
+
     public bool IsFlipped = false;
+
+    public void Start()
+    {
+        originalColour = GetComponent<Renderer>().material.color;
+        UpdateVisual();
+    }
+
+    public void Flip()
+    {
+        IsFlipped = !IsFlipped;
+        UpdateVisual();
+    }
+
+    private void UpdateVisual()
+    {
+        // Update the visual representation of the piece based on IsFlipped
+        // This is a placeholder implementation; actual implementation may vary
+        if (IsFlipped)
+        {
+            // Change to flipped appearance
+            GetComponent<Renderer>().material.color = Color.cadetBlue;
+        }
+        else
+        {
+            // Change to normal appearance
+            GetComponent<Renderer>().material.color = originalColour;
+        }
+    }
 }
